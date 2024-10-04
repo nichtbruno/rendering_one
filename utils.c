9c03e2623346c02dbf11ef66b68280b0e8ac335e
Bruno Popic <brunopopifv@gmail.com>
2024-10-04 23:29:37 +0200

    tested Makefile for headers

---
+++ utils.c
@@ -0,0 +1,8 @@
+#include "utils.h"
+
+int stringL(char *s) {
+    int i = 0;
+    while(s[i++])
+        ;;
+    return i-1;
+}
---
+++ utils.h
@@ -0,0 +1,25 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * @brief Calculates the length of a null-terminated string.
+ *
+ * This function counts the number of characters in a string up to,
+ * but not including, the null terminator ('\0').
+ *
+ * @param s Pointer to the null-terminated string to be measured.
+ * @return The number of characters in the string, excluding the null terminator.
+ *
+ * @note This function assumes that the input string is properly null-terminated.
+ * @warning Passing a non-null-terminated string may result in undefined behavior.
+ *
+ * Example usage:
+ * @code
+ * char *myString = "Hello, World!";
+ * int length = stringL(myString);
+ * printf("The length of the string is: %d\n", length);
+ * @endcode
+ */
+int stringL(char *s);
+
+#endif
